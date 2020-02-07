c01b6c7a4152ec0f004284f6251f07780516db96
gizmore <gizmore@wechall.net>
2020-02-07 08:05:38 +0100

    6.10-r5009_bootstrap_und_mettwitze

--- GDO_MettwitzVote.php
+++ GDO_MettwitzVote.php
@@ -7,7 +7,7 @@ final class GDO_MettwitzVote extends GDO_VoteTable
 {
 	public function gdoVoteObjectTable() { return GDO_Mettwitz::table(); }
 	public function gdoVoteMax() { return 5; }
-	public function gdoVotesBeforeOutcome() { return 1; }
+	public function gdoVotesBeforeOutcome() { return 3; }
 	public function gdoVoteCooldown() { return 60*60*24; }
 	public function gdoVoteGuests() { return Module_Mettwitze::instance()->cfgGuestVotes(); }
 	
