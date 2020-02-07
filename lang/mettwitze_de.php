2c37d42179199e40555452573c303c6e75faaa46
gizmore <gizmore@wechall.net>
2020-02-07 07:29:52 +0100

    6.10-r5009_bootstrap_und_mettwitze

---
+++ lang/mettwitze_de.php
@@ -0,0 +1,31 @@
+<?php
+return array(
+'lbl_mettwitze' => 'Mettwitze',
+'link_witze' => 'Mettwitze',
+'link_add_witz' => 'Neuer Mettwitz',
+
+'paragraph_mettwitze' => 'Eine Sammlung von Mettwitzen. Nur in Deutsch. Klicke um die Auflösung aufzudecken.',
+'list_listwitze' => '%s Mettwitze',
+'witz_meta' => '%s von %s',
+'witz_comments' => '%s <a href="%s">Kommentare</a>',
+	
+'gdo_mettwitz' => 'Mettwitz',
+'mw_question' => 'Frage',
+'mw_answer' => 'Antwort',
+	
+'ft_mettwitze_addcomment' => 'Kommentar schreiben',
+	
+'mail_subj_new_mettwitz' => 'Neuer Mettwitz',
+'mail_body_new_mettwitz' => '
+Hallo %s,
+
+Ein neuer Mettwitz wurde hinzugefügt.
+
+Frage: %s
+
+Antwort: %s
+
+Viele Grüße
+Das %s Team
+',
+);
